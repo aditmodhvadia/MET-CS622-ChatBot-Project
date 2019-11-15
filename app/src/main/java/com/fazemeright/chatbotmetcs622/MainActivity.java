89d38c8be1b4ef2bc2151caaa7b88348cfd2a4c6
Adit Modhvadia <adit.modhvadia@gmail.com>
2019-11-14 23:46:42 -0500

    Initial project setup

---
+++ app/src/main/java/com/fazemeright/chatbotmetcs622/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fazemeright.chatbotmetcs622;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
